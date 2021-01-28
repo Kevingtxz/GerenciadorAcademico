6773a63a86330ae0c19532f42c98025e86c226df
Kevin <kevingtxz@gmail.com>
2021-01-28 19:28:22 -0300

    DockerFile

---
+++ gerenciadoracademico/src/main/java/com/kevin/gerenciadoracademico/GerenciadoracademicoApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.gerenciadoracademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadoracademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadoracademicoApplication.class, args);
+	}
+
+}
