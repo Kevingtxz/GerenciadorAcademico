5f043bb4abf5e543e78b2b4d9dd7f07d580dc4aa
Kevin <kevingtxz@gmail.com>
2021-01-29 12:07:01 -0300

    mongodb funcionando

--- gerenciadoracademico/src/main/java/com/kevin/gerenciadoracademico/domain/User.java
+++ gerenciadoracademico/src/main/java/com/kevin/gerenciadoracademico/domain/User.java
@@ -2,11 +2,16 @@ package com.kevin.gerenciadoracademico.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 // Construí essa classe para ser o usuário do serviço, tudo com o mínimo de complexidade
 
+@Document(collection="user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
 	private String id;
 	private String name;
 	private String email;
---
+++ gerenciadoracademico/src/main/java/com/kevin/gerenciadoracademico/services/UserService.java
@@ -0,0 +1,20 @@
+package com.kevin.gerenciadoracademico.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kevin.gerenciadoracademico.domain.User;
+import com.kevin.gerenciadoracademico.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repo; 
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
